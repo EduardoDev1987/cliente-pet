abd5a7e13e1afae21672b037b14ea6922d24002c
Eduado Silva <eduardops87@gmail.com>
2025-05-20 19:09:53 -0300

    implementa contrato da api

---
+++ src/main/java/br/com/petz/cliente_ptez/cliente/application/api/ClienteResponse.java
@@ -0,0 +1,5 @@
+package br.com.petz.cliente_ptez.cliente.application.api;
+
+public class ClienteResponse {
+
+}
--- src/main/java/br/com/petz/cliente_ptez/cliente/domain/Sexo.java
+++ src/main/java/br/com/petz/cliente_ptez/cliente/domain/Sexo.java
@@ -1,5 +1,5 @@
 package br.com.petz.cliente_ptez.cliente.domain;
 
 public enum Sexo {
-
+    FEMININO,MASCULINO;
 }
