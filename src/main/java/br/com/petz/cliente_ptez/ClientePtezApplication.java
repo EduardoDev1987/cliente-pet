0467b708dd50dec88f03e6fe6dc83e6b5f601636
Eduado Silva <eduardops87@gmail.com>
2025-05-20 10:39:39 -0300

    inicia o projeto

---
+++ src/main/java/br/com/petz/cliente_ptez/ClientePtezApplication.java
@@ -0,0 +1,13 @@
+package br.com.petz.cliente_ptez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePtezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePtezApplication.class, args);
+	}
+
+}
