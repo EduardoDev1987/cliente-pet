923a9fd79fecdb90bfd5f3b6c48fab32affa6c4a
Eduado Silva <eduardops87@gmail.com>
2025-05-22 09:59:12 -0300

    implementa correcao ClienteApplicationService e clienteRequest

---
+++ src/main/java/br/com/petz/cliente_ptez/cliente/application/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package br.com.petz.cliente_ptez.cliente.application.repository;
+
+import br.com.petz.cliente_ptez.cliente.domain.Cliente;
+
+public interface ClienteRepository {
+	Cliente salva(Cliente cliente);
+
+}
