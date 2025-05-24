22fd1e0c96b01a5f083f40018a9a5c13afcedd4b
Eduado Silva <eduardops87@gmail.com>
2025-05-23 22:48:28 -0300

    implementa alteracao camada API

--- src/main/java/br/com/petz/cliente_ptez/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/cliente_ptez/cliente/application/api/ClienteController.java
@@ -3,9 +3,12 @@ package br.com.petz.cliente_ptez.cliente.application.api;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.petz.cliente_ptez.cliente.application.service.ClienteService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -47,6 +50,13 @@ public class ClienteController implements ClienteAPI {
 		log.info("[idCliente]{}", idCliente);
 		clienteService.deletaClienteAtravesId(idCliente);
 		log.info("[finaliza] ClienteController  deletaClienteAtravesId");
+	}
+
+	@Override
+	public void patchAlteraCliente(UUID idCliente, @Valid ClienteAlteracaoRequest clienteAlteracaoRequest) {
+		log.info("[inicia] ClienteController  patchAlteraCliente");
+		log.info("[idCliente]{}", idCliente);
+		log.info("[finaliza] ClienteController  patchAlteraCliente");
 	}
 
 	
