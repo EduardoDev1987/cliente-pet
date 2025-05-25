7eb4d23245ccb098e8813311ec1b7c9bf18082f8
Eduado Silva <eduardops87@gmail.com>
2025-05-25 15:55:55 -0300

    implementa repository infra pet busca pets por idcliente

--- src/main/java/br/com/petz/cliente_ptez/pet/infra/PetSpringDataJPARepository.java
+++ src/main/java/br/com/petz/cliente_ptez/pet/infra/PetSpringDataJPARepository.java
@@ -1,5 +1,6 @@
 package br.com.petz.cliente_ptez.pet.infra;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.petz.cliente_ptez.pet.domain.Pet;
 
 public interface PetSpringDataJPARepository extends JpaRepository<Pet, UUID>{
+	List<Pet> findByIdClienteTutor(UUID idClienteTutor);
 
 }
